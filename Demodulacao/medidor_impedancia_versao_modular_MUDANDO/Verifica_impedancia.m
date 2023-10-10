87567f9d14c9bfc5e5c68fc228445206f6fcf2f7
pinheirogustavo <gustavopinheirozz@gmail.com>
2023-10-10 18:28:49 -0300

    versao sendo atualizada

--- Demodulacao/medidor_impedancia_versao_modular_MUDANDO/Verifica_impedancia.m
+++ Demodulacao/medidor_impedancia_versao_modular_MUDANDO/Verifica_impedancia.m
@@ -1,11 +1,11 @@
 ### A partir dos valores de amplitude e fase de Z1(canal 1 do medidor)
 ### e Z2(canal 2 do medidor), calcula Z (z1-z2), apresentando modulo e fase
 
-modulo_z1 = 1101.30;
-theta1 = -0.16  ; #graus
+modulo_z1 = 767.97;
+theta1 = 9  ; #graus
 theta1 = deg2rad(theta1);
-modulo_z2 = 1003.43;
-theta2 = 0.24; #graus
+modulo_z2 = 9.24;
+theta2 = -18.87; #graus
 theta2 = deg2rad(theta2);
 
 z1 = modulo_z1*exp(i*theta1);
@@ -19,7 +19,7 @@ theta_z = rad2deg(theta_z)
 
 sdsd = atan2(imag(z), real(z))
 sdsd = rad2deg(sdsd);
-
+reatancia = imag(z)
 
 
 ffff = atan2( (modulo_z1*sin(theta1) - modulo_z2*sin(theta2)) , (modulo_z1*cos(theta1) - modulo_z2*cos(theta2)))
