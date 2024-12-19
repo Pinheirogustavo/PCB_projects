e42c0fad0b6f0d2c2fe8c350d7dc5c6f7891e020
pinheirogustavo <gustavopinheirozz@gmail.com>
2024-12-19 14:27:51 -0300

    teste

---
+++ KiCadProjects/Gerador_sinais/AD9850/V01/TESTE/gerador.h
@@ -0,0 +1,157 @@
+//#Autor: Gustavo Pinheiro
+
+
+//freq inicial
+int freq_sinal = 100;
+// frequencias a serem testadas
+int freq[37] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000,  100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+int i=0;
+//#include "ad9850.h"
+
+int Rs; //valor do resistor variavel
+
+//-----------------------------------------DigiPot1-------------------------------------//
+
+// Saidas digitais
+#define inc_outPin PB11 // INC(DigPot1)
+#define ud_outPin PB10  // U/D(DigPot1)
+#define cs_digpot1 PB1   // CS do DigPot 1, usado para salvar a ultima resistencia obtida
+
+const int loopPeriod = 50;
+
+
+
+//-----------------------------------------DigiPot2-------------------------------------//
+
+
+//-----------------------------------------Encoder-------------------------------------//
+
+
+//-----------------------------------------Teclado matricial-------------------------------------//
+
+
+///////////////// FUNCOES /////////////////////
+
+
+
+void incrementa_frequencia(){
+    if (freq_sinal < 1000000){
+        freq_sinal = freq[i+1];
+        i = i+1;
+        ad9850_sendFrequency(freq_sinal);
+        delay(loopPeriod);
+        Serial.print("Incremento do sinal\t");
+        Serial.print("frequencia: ");
+        Serial.println(freq_sinal);
+    } else Serial.println("frequencia maxima atingida");    
+}
+
+void decrementa_frequencia(){
+    if (freq_sinal >= 100){
+        freq_sinal = freq[i-1];
+        i = i-1;
+        ad9850_sendFrequency(freq_sinal);
+        delay(loopPeriod);
+        Serial.print("Decremento do sinal\t");
+        Serial.print("frequencia: ");
+        Serial.println(freq_sinal);
+    }
+    else Serial.println("frequencia minima atingida");
+}
+
+
+
+void wiper_down(){ //wiper down
+  digitalWrite(ud_outPin, LOW);
+  delay(loopPeriod);
+  digitalWrite(inc_outPin, LOW);
+  delay(loopPeriod);
+  digitalWrite(inc_outPin, HIGH);
+  delay(loopPeriod);
+  digitalWrite(ud_outPin, HIGH);  
+}
+
+void aumenta_ganho(){//wiper down
+    if (Rs>0){
+        wiper_down();
+        delay(loopPeriod);
+        Rs = Rs-1;
+        Serial.print("Incremento da amplitude do sinal\t ");
+        Serial.print("valor do digpot Rs(kOhm): ");
+        Serial.println(Rs);
+    }else Serial.println("ganho maximo atingido");
+
+}
+
+
+void wiper_up(){ //wiper up
+  digitalWrite(ud_outPin, HIGH);
+  delay(loopPeriod);
+  digitalWrite(inc_outPin, LOW);
+  delay(loopPeriod);
+  digitalWrite(inc_outPin, HIGH);
+  delay(loopPeriod);
+}
+
+void diminui_ganho(){ //wiper up
+    if (Rs<100){
+        wiper_up();
+        delay(loopPeriod);
+        Rs = Rs+1;
+        Serial.print("Decremento da amplitude do sinal\t");
+        Serial.print("valor do digpot Rs(kOhm): ");
+        Serial.println(Rs);
+    } else Serial.println("ganho minimo atingido");    
+}
+
+
+void ganho_minimo(){
+    while (Rs<100){
+        Serial.println("Configurando ganho minimo...");
+        diminui_ganho();
+        Rs=Rs+1;
+    } 
+    Serial.println("ganho minimo atingido");  
+}
+
+void ganho_maximo(){
+    while (Rs>0){
+        Serial.println("Configurando ganho maximo...");
+        aumenta_ganho();
+        Rs=Rs-1;
+    } 
+    Serial.println("ganho maximo atingido");  
+}
+
+
+
+void inicia_digpot(){
+  for (int i = 0; i < 99; i++) {
+        diminui_ganho();
+  }
+  Serial.println("ganho minimo atingido");  
+  Rs = 100;
+}
+
+void printa_dados(){
+    Serial.print("valor de Rs: ");
+    Serial.println(Rs);
+    Serial.print("Frequencia atual: ");
+    Serial.println(freq_sinal);  
+}
+
+
+
+void gerador_setup() {
+    Serial.begin(9600);
+
+    pinMode(inc_outPin, OUTPUT);
+    pinMode(ud_outPin, OUTPUT);
+    pinMode(cs_digpot1, OUTPUT);
+
+    digitalWrite(inc_outPin, HIGH);
+    digitalWrite(ud_outPin, HIGH);
+    digitalWrite(cs_digpot1, LOW);
+    
+    //inicia_digpot();
+}
---
+++ KiCadProjects/Gerador_sinais/AD9850/V01/TESTE/gerador_array_serial.ino
@@ -0,0 +1,94 @@
+//#Autor: Gustavo Pinheiro
+/*Programa para controle do projeto "Gerador de sinais AD9850 V01" por meio da porta serial da placa stm
+ * As frequencias possiveis sao aquelas presentes no vetor freq
+ * O incremento e decremento do vetor de frequencias eh realizado por meio dos caracteres "+" e "-"
+ * O aumento da amplitude do sinal eh feito pelo caracter "u"
+ * A diminuicao da amplitude do sinal eh feita pelo caracter "d" 
+ */
+#include "ad9850.h"
+#include "gerador.h"
+
+#include "Arduino.h"
+#include "X9C10X.h"
+
+X9C10X pot(100000);  //  100 KΩ  (ALT-234)
+
+uint8_t direction = LOW;
+uint8_t step = 1;
+
+
+//const int loopPeriod = 50;  
+char comando;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Aguarde a configuracao do sistema");
+
+
+  ad9850_setup(); 
+  ad9850_sendFrequency(freq_sinal);
+  gerador_setup();
+  delay(1000);
+
+
+  pot.begin(inc_outPin, ud_outPin, cs_digpot1);  // pulse, direction, select
+  pot.setPosition(0);
+
+
+  Serial.println("Configuracao encerrada");
+
+}
+  
+void loop() {
+
+  if(Serial.available()){
+    comando = Serial.read();
+
+    switch (comando) {
+
+      //case 'i':
+
+      case '+': // incrementa o vetor de frequencias
+        incrementa_frequencia();
+        break;     
+
+      case '-': // decrementa o vetor de frequencias
+        decrementa_frequencia();
+        break; 
+        
+        
+      case 'd': // diminui a amplitude do sinal (wiper up)
+        pot.decr();
+        delay(100);
+        break;
+        
+      case 'u': // aumenta a amplitude do sinal (wiper down)
+        pot.incr();
+        delay(100);
+        break;
+      
+      case 's': // Salva a resistencia
+        digitalWrite(cs_digpot1, HIGH);
+        delay(loopPeriod);
+        digitalWrite(cs_digpot1, LOW);
+        break;  
+
+      case 'x':
+        ganho_minimo();
+        break;
+
+      case 'y':
+        ganho_maximo();
+        break;
+
+      case 'p':
+        printa_dados();
+        break;
+
+    default:
+        break;
+          
+    }
+  }        
+}
