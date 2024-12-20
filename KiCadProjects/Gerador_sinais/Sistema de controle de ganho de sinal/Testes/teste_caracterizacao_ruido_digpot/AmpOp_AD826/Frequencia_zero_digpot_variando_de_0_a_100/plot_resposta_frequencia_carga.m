3a0961946a4c76c238a406d52385efb2be80264b
gustavo <gustavopinheirozz@gmail.com>
2024-12-20 16:00:59 -0300

    teste com ad826 freq zero todos passos digpot

---
+++ KiCadProjects/Gerador_sinais/Sistema de controle de ganho de sinal/Testes/teste_caracterizacao_ruido_digpot/AmpOp_AD826/Frequencia_zero_digpot_variando_de_0_a_100/plot_resposta_frequencia_carga.m
@@ -0,0 +1,120 @@
+function [] = resposta_frequencia()
+% AINDA NAO FINALIZADA E TESTADA
+
+  predefinicoes;
+
+indice_polinomio = 3;
+
+numero_curvas = input('Digite o numero de curvas a serem analisadas: ');
+
+for i=1:length(numero_curvas)
+  nome_arquivo_sumario = input('Digite o nome do arquivo com os dados sumarizados de uma curva: ', 's');
+  nome_arquivo_sumario = strcat(nome_arquivo_sumario,'.csv');
+  dados(:,i) = csvread(nome_arquivo_sumario);
+  disp('Proxima curva...');
+end
+disp('Finalizado o carregamento das curvas');
+
+tamanho_dados_linha = size(dados(1));
+numero_analises = floor(idx/12); %quantidade de analises presentes nos dados
+idx_amplitude_tensao = 8; %linha em que comecam os valores de amplitude do sinal obtido
+
+for idx=1:numero_analises
+  tensao_carga(idx,1) = dados(idx_amplitude_tensao,1) %amplitude da tensao sobre a carga
+  idx_amplitude_tensao = idx_amplitude_tensao+8; %passa para a proxima amplitude
+end
+
+selecao = input('As curvas serao levantadas em funcao da carga (l) ou frequencia (f)?');
+
+switch selecao
+
+  case 'l'
+    for i=1:length(numero_curvas)
+      nome_arquivo_carga = input('Digite o nome do arquivo com os dados de cargas testadas, na sequencia em que as curvas foram adicionadas no programa: ', 's');
+      nome_arquivo_carga = strcat(nome_arquivo_carga,'.csv');
+      vetor_carga(:,i) = csvread(nome_arquivo_carga);
+      disp('Proximo dado de carga...');
+
+      Iload(:,i) =( (dados(:,i)./vetor_x(:,i))*1000 );#corrente atraves da carga (mA)
+      p(:,i) = polyfit(vetor_carga(:,i), Iload(:,i), indice_polinomio); #devolve os coeficientes do polinomio, ultimo argumento eh o grau
+    end
+
+    disp('Finalizado o carregamento das cargas');
+    vetor_x = vetor_carga
+    grafico = 'l'; %indica que o grafico gerado sera em funcao da carga
+
+  case 'f'
+    vetor_x = frequencia sinal salvo na funcao sumariza varios
+
+    grafico = 'f'; %indica que o grafico gerado sera em funcao da frequencia
+
+end
+
+
+xmin = vetor_x(1);
+xmax = vetor_x(length(vetor_x));
+
+xx = xmin:0.1:xmax; #vetor de x
+
+
+ for i=1:length(numero_curvas)
+    yy(:,i) = polyval(p(:,i),xx); #vetor de y
+ end
+
+
+ switch grafico
+
+ case 'l'
+
+    loglog(xx,yy,'r', 'linewidth', 2, cargas, Iload, 'k*');
+ end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+figure(1); hold on
+loglog(xx,yy,'r', 'linewidth', 2, cargas, Iload, 'k*');
+loglog(xx_1,yy_1,'b', 'linewidth', 2, cargas_1, Iload_1, 'g*');
+loglog(xx_2,yy_2,'c', 'linewidth', 2, cargas_2, Iload_2, 'm*');
+
+line ([38000 38000], [0.1 4], "linestyle", ":", "color", "r", 'linewidth', 2);
+line ([5490 5490], [0.1 4], "linestyle", ":", "color", "b", 'linewidth', 2);
+line ([1094 1094], [0.1 4], "linestyle", ":", "color", "c", 'linewidth', 2);
+ #ponto em que ocorre saturacao do ampOp SEMPRE ALTERAR
+
+
+gggggggggggggg = text(22000, 1.5,['38k \Omega \rightarrow  '])
+ggggggggggg = text(6000, 1.5,['\leftarrow  5.5k \Omega  '])
+ggggggggg = text(700, 1.5,['1k \Omega \rightarrow  '])
+
+
+
+#caso queira imprimir o polinomio no gráfico:
+s = sprintf('y =(%.1f) x^2 +(%.5f) x + (%.5f)',p(1),p(2), p(3));
+#text(100,0.4,s) #posição do polinomio no gráfico
+
+title('Resposta da fonte de corrente em função da carga - 50 kHz');
+ylabel('Iload[mA]');
+xlabel('cargas[Ω]');
+h = legend ("ajuste de curva i=0.5 mA","pontos experimentais i=0.5 mA",
+"ajuste de curva i=1.0 mA","pontos experimentais i=1.0 mA",
+"ajuste de curva i=3.3 mA","pontos experimentais i=3.3 mA",
+"saturação do AmpOp i=0.5 mA",
+"saturação do AmpOp i=1.0 mA",
+"saturação do AmpOp i=3.3 mA");
+
+legend(h,'Location','southwest'	);
+xlim ([xmin,40000])
+ylim ([0.1,4])
+
+
+grid on
+
+saveas(1,'correnteXcarga.jpg')
+
+#coeficientes do polinomio
+p(1)
+p(2)
+
+
+end
---
+++ KiCadProjects/Gerador_sinais/Sistema de controle de ganho de sinal/Testes/teste_caracterizacao_ruido_digpot/AmpOp_AD826/Frequencia_zero_digpot_variando_de_0_a_100/sumariza_dados.m
@@ -0,0 +1,99 @@
+function  [] = sumariza_dados()
+%[] = sumariza_dados()
+  %
+  %funcao 'sumariza_dados': permite obter sumario dos dados obtidos por osciloscopio.
+  %
+  %Retorna: nada.
+  %
+  %Exibe no prompt de comando os seguintes dados do canal desejado:
+    % - Tensao pico a pico (V);
+    % - Amplitude do sinal(V);
+    % - Tensao RMS(V);
+    % - Tensao maxima(V);
+    % - Tensao minima(V);
+    % - Tensao media(V);
+    % - Em relacao aos picos da fft considerados na analise (>20% tensao maxima):
+          % - Amplitude(V) e frequencia(kHz).
+    %
+    % Gera um arquivo 'nome_do_arquivo_sumario.txt' com todos os dados exibidos no prompt de comando.
+  %
+  %ATENCAO: O arquivo .txt gerado adiciona novos dados abaixo dos ja existentes
+  %no arquivo .txt presente no mesmo diretorio.
+   %%%%
+    %%%%
+    %%%%
+    #Autor: Gustavo Pinheiro
+    #email: gustavo.pinheiro.ebm@gmail.com / gustavopinheirozz@gmail.com
+    #versao octave: 8.0
+
+  predefinicoes;
+
+  [dados_original,vetor_tempo,vetor_tempo_plot,vetor_tensao,vetor_tensao_plot,Ts,Fs,L,MinPeakHeight,nome_arquivo] = abre_dados;
+
+canal = input('Digite o numero do canal a ser analisado: ');
+
+nome_txt = strcat(nome_arquivo,'_sumario.txt'); %comandos para salvar txt
+diary(nome_txt)                                 %com os resultados sumarizados
+diary on
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Tensao pico a pico%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%PeakToPeak_Amplitude = max(abs(dados_original))*2 - mean(dados_original)*2
+pico_a_pico = peak2peak(vetor_tensao(:,canal));
+fprintf('tensao pico a pico(V): %f\n', pico_a_pico);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% amplitude %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('amplitude do sinal(V): %f\n', pico_a_pico/2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Tensao rms%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+RMS_Amplitude = sqrt(mean(vetor_tensao(:,canal).^2));
+fprintf('tensao RMS(V): %f\n', RMS_Amplitude);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% maximo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+maximo = max(vetor_tensao(:,canal));
+fprintf('tensao maxima(V): %f\n', maximo);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% minimo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+minimo = min(vetor_tensao(:,canal));
+fprintf('tensao minima(V): %f\n', minimo);
+%%%%%%%%%%%%%%%%%%%%%%%%% media %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+media = mean(vetor_tensao(:,canal));
+fprintf('tensao media(V): %f\n', media);
+
+%%%%%%%%%%%%%%%%%%%%%%%%% frequencias presentes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  Y = fft(vetor_tensao(:,canal)); %computes the discrete Fourier transform of vetor_tensao
+
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1 = P2(1:L/2+1).*1000;       %em mX;
+    P1(2:end-1) = 2*P1(2:end-1);
+
+    f = Fs/L*(0:(L/2));
+
+    MinPeakHeight=(0.02*maximo).*1000; %tamanho minimo para o pico ser detectado
+                                              %(20% tensao maxima)
+    [pks,locs] = findpeaks(P1,"MinPeakHeight",MinPeakHeight);
+        %{
+        retorna os picos (pks) e suas posicoes (locs) do sinal
+        %dois picos consecutivos sao distintos se a distancia entre eles for maior
+        do que 2 segundos
+        %}
+
+    fprintf('Componentes presentes significativas (20@amplitudeMaxima): \n');
+
+     idx = length(pks);
+     for i = 1:idx
+       pico =  f(locs(i))/1000 ;%kHz
+
+       fprintf('frequencia (kHz): %f\n', pico);
+       fprintf('amplitude (V): %f\n', pks(i)/1000);
+     end
+
+
+diary off
+
+end
+
+
